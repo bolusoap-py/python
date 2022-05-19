87565b67f9459495cb62e92b698c13043b2b28ea
bolusoap-py <100145058+bolusoap-py@users.noreply.github.com>
2022-05-20 00:54:43 +0100

    Update soap.py

--- soap.py
+++ soap.py
@@ -1,3 +1,4 @@
+print(" THIS IS A CALCULATOR  ")
 num1 = float(input(" Enter first number: "))
 op = input("Enter operator:")
 num2 = float(input(" Enter second number: "))
@@ -11,5 +12,6 @@ elif op == "*":
     print(num1 * num2)
 else:
     print("syntax error")
+    
     
 
