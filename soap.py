49a4193cd641998a7745f77ac8e0f4afcc8c5cef
bolusoap-py <100145058+bolusoap-py@users.noreply.github.com>
2022-05-20 00:59:41 +0100

    add title

--- soap.py
+++ soap.py
@@ -1,4 +1,4 @@
-print(" THIS IS A CALCULATOR  ")
+print(" THIS IS A CALCULATOR: ")
 num1 = float(input(" Enter first number: "))
 op = input("Enter operator:")
 num2 = float(input(" Enter second number: "))
